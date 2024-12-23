c0aaeef42b8757658307abb1b6ef3ffa5bcd4801
Grace Lattanzio <gralatta@gmail.com>
2024-12-22 22:59:25 -0600

    Checks health and code visability update

--- main.lua
+++ main.lua
@@ -1,24 +1,52 @@
+-- Check to ensure that it's a member of the group/raid
+local function IsInGroup(unit)
+    return UnitInParty(unit) or UnitInRaid(unit) or UnitIsUnit(unit, "pet")
+end
+
 -- Function to check mana and print a warning if low
 local function CheckMana()
     local maxMana = UnitPowerMax("player", 0)
     local currentMana = UnitPower("player", 0)
+    local manaPercentage = (currentMana / maxMana) * 100
 
-    if currentMana / maxMana <= 0.25 then
+    if manaPercentage > 35 then
+        manaMessageCount = 0
+    elseif manaPercentage <= 25 then
         SendChatMessage("Warning: Critically low mana", "PARTY")
-    elseif currentMana / maxMana <= 0.35 then
+        manaMessageCount = 1
+    elseif manaPercentage <= 35 and manaMessageCount <= 2 then
         SendChatMessage("Warning: Mana Low", "PARTY")
+        manaMessageCount = manaMessageCount + 1
+    end
+end
+
+-- Function to check health and print a warning if low
+local function CheckHealth(unit)
+    local maxHealth = UnitHealthMax(unit)
+    local currentHealth = UnitHealth(unit)
+    local healthPercentage = (currentHealth / maxHealth) * 100
+
+    if healthPercentage <= 30 then
+        SendChatMessage("WARNING: " .. unit .. " needs heals!!", "PARTY")
+        healthMessageCount = 1
+    elseif healthPercentage <= 50 and healthMessageCount <= 3 then
+        SendChatMessage(unit .. " is low", "PARTY")
+        healthMessageCount = healthMessageCount + 1
     end
 end
 
 -- Register events to update mana and health on power change and heal prediction
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("UNIT_POWER_UPDATE")
+frame:RegisterEvent("UNIT_HEALTH")
+
+manaMessageCount = 0
+healthMessageCount = 0
 
 frame:SetScript("OnEvent", function(self, event, arg1, arg2)
     if arg1 == "player" and event == "UNIT_POWER_UPDATE" and arg2 == "MANA" then
         CheckMana()
+    elseif event == "UNIT_HEALTH" and IsInGroup(arg1) == true then
+        CheckHealth(arg1)
     end
 end)
-
--- Also check the mana on login
-CheckMana()
